cda2cb8ab6fbae67a4af44be25719803dd48d385
Michael Brenndoerfer <Brenndoerfer@users.noreply.github.com>
2024-04-25 13:27:40 +0800

    test husky pre-commit

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Page(): JSX.Element {
 
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24 bg-red-200">
-      Hello Test
+      Hello Test husky
     </main>
   );
 }
